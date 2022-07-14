cb8c62cc72d82407f5770ccba7974189b1470cf9
Quincy Cheng <quincy.cheng@gmail.com>
2022-07-14 11:18:23 +0800

    adding Jenkins

--- conjur-jenkins/courseBase.sh
+++ conjur-jenkins/courseBase.sh
@@ -1,12 +1,63 @@
 #!/bin/bash
 
-# clean up, dev only, should be done in image
-rm -rf *
+#docker-compose.yml
+cat <<EOF > docker-compose.yml
+version: '3'
+services:
+  database:
+    image: postgres:10.16
+    container_name: postgres_database
+    environment:
+      POSTGRES_HOST_AUTH_METHOD: trust
+    ports:
+      - 8432:5432
+  conjur:
+    image: cyberark/conjur
+    container_name: conjur_server
+    command: server
+    environment:
+      DATABASE_URL: postgres://postgres@database/postgres
+      CONJUR_DATA_KEY:
+      CONJUR_AUTHENTICATORS:
+    depends_on:
+    - database
+    restart: on-failure
+    ports:
+      - 8080:80
+
+  client:
+    image: conjurinc/cli5
+    depends_on: [ conjur ]
+    entrypoint: sleep
+    command: infinity
+    environment:
+      CONJUR_APPLIANCE_URL: http://conjur
+      CONJUR_ACCOUNT:
+      CONJUR_AUTHN_API_KEY:
+      CONJUR_AUTHN_LOGIN: admin
+
+  jenkins:
+    image: jenkins/jenkins:lts
+    privileged: true
+    user: root
+    ports:
+      - 8081:8080
+      - 50000:50000
+    container_name: jenkins
+    volumes:
+      - /root/katacoda-env-conjur-jenkins/jenkins_home:/var/jenkins_home
+      - /var/run/docker.sock:/var/run/docker.sock
+      - /usr/local/bin/docker:/usr/local/bin/docker
+
+  http-auth-server:
+    image: quincycheng/http-authn-server:latest
+    ports:
+     - 8082:80
+    container_name: http-auth-server
+EOF
+
+
+# Get jenkins_home
 git clone https://github.com/quincycheng/katacoda-env-conjur-jenkins.git
-
-cd katacoda-env-conjur-jenkins
-
-export CONJUR_DATA_KEY="$(docker-compose run --no-deps --rm conjur data-key generate)"
-docker-compose up -d 
-
-echo "$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' conjur_conjur_1) conjur" >> /etc/hosts
+mv katacoda-env-conjur-jenkins/jenkins_home .
+rm -rf katacoda-env-conjur-jenkins

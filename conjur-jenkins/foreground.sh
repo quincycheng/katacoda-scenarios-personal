3a21f639c7b90f0593a5f9609795144b838e117e
Quincy Cheng <quincy.cheng@gmail.com>
2022-07-14 15:59:32 +0800

    conjur jenkins

--- conjur-jenkins/foreground.sh
+++ conjur-jenkins/foreground.sh
@@ -4,5 +4,5 @@ clear && printf "Preparing environment...\n- docker-compose.yml..." && \
 sleep 1s && \
 timeout 30s bash -c 'while ! [ -f /root/docker-compose.yml ];do printf ".";sleep 2s;done'  && \
 printf "✅\n- Jenkins files..." && \
-timeout 120s bash -c 'while ! [ -f /root/jenkins_home/conjur.xml ];do printf ".";sleep 2s;done'   && \
+timeout 120s bash -c 'while ! [ -f /root/jenkins_home/config.xml ];do printf ".";sleep 2s;done'   && \
 echo -e "✅\n- Ready! 😀"

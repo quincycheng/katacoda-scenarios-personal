e642fb215c903f1aef178c756ba92b19841119f2
Quincy Cheng <quincy.cheng@gmail.com>
2022-07-15 14:16:32 +0800

    conjur k8s secretless

--- conjur-k8s-secretless/background.sh
+++ conjur-k8s-secretless/background.sh
@@ -1,9 +1,7 @@
 #!/bin/bash 
 
-kubectl taint nodes --all node.kubernetes.io/disk-pressure-
-
 git clone https://github.com/quincycheng/katacoda-secretless-files.git > /dev/null 2>&1 && \
 mv katacoda-secretless-files/ . && rm -rf katacoda-secretless-files \
-apt install -y wamerican & && \
+apt install -y wamerican && \
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash && \
 clear

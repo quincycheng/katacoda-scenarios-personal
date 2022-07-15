923a33fc291fbb25bd585a52d0bd3c637bdba4ff
Quincy Cheng <quincy.cheng@gmail.com>
2022-07-15 14:11:14 +0800

    conjur k8s secretless

--- conjur-k8s-secretless/background.sh
+++ conjur-k8s-secretless/background.sh
@@ -3,7 +3,7 @@
 kubectl taint nodes --all node.kubernetes.io/disk-pressure-
 
 git clone https://github.com/quincycheng/katacoda-secretless-files.git > /dev/null 2>&1 && \
-cd katacoda-secretless-files/ && \
-apt install -y wamerican && \
+mv katacoda-secretless-files/ . && rm -rf katacoda-secretless-files \
+apt install -y wamerican & && \
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash && \
 clear
---
+++ conjur-k8s-secretless/foreground.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set +H
+clear && printf "Verifying environment...\n- Preparing App files..." && \
+sleep 1s && \
+timeout 30s bash -c 'while ! [ -f /root/db/db.yml ];do printf ".";sleep 2s;done'  && \
+printf "✅\n- Installing Dictionary..." && \
+timeout 60s bash -c 'while [ -f /usr/share/dict/american-english ];do printf ".";sleep 2s;done'   && \
+printf "✅\n- Target Host 2..." && \
+timeout 60s bash -c 'while [ ! $(command -v helm) ];do printf ".";sleep 2s;done'   && \
+echo -e "✅\n- Ready! 😀"
+
+

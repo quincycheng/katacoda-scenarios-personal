3e1f228f1c37ff26e88bdb5f051eea3b5892d284
Quincy Cheng <quincy.cheng@gmail.com>
2022-07-15 14:20:42 +0800

    conjur k8s secretless

--- conjur-k8s-secretless/background.sh
+++ conjur-k8s-secretless/background.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
 git clone https://github.com/quincycheng/katacoda-secretless-files.git > /dev/null 2>&1 && \
-mv katacoda-secretless-files/ . && rm -rf katacoda-secretless-files \
-apt install -y wamerican && \
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash && \
-clear
+mv katacoda-secretless-files/* . && \
+rm -rf katacoda-secretless-files/
+
+apt install -y wamerican 
+
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash 

576f25d88e72c07fa31d137a43c6fbd567f47d95
Quincy Cheng <quincy.cheng@gmail.com>
2022-07-18 16:59:06 +0800

    conjur jenkins

--- conjur-jenkins/foreground.sh
+++ conjur-jenkins/foreground.sh
@@ -3,16 +3,18 @@ set +H
 
 date
 
-clear && printf "Preparing environment...\n- docker-compose.yml..." && \
+clear && date && \
+printf "Preparing environment, it will take 3-5mins...\n- Getting docker-compose.yml..." && \
 sleep 1s && \
 timeout 30s bash -c 'while ! [ -f /root/docker-compose.yml ];do printf ".";sleep 2s;done'  && \
+printf "✅\n- Downloading Conjur policy files..." && \
+timeout 120s bash -c 'while ! [ -f /root/jenkins-app.entitled.yml ];do printf ".";sleep 2s;done'   && \
 printf "✅\n- Downloading Jenkins files..." && \
 timeout 120s bash -c 'while ! [ -f /root/jenkins_home/config.xml ];do printf ".";sleep 2s;done'   && \
 printf "✅\n- Installing Python PIP..." && \
 timeout 60s bash -c 'while [ ! $(command -v pip) ];do printf ".";sleep 2s;done'   && \
 printf "✅\n- Installing Conjur CLI..." && \
 timeout 60s bash -c 'while [ ! $(command -v conjur) ];do printf ".";sleep 2s;done'   && \
-
 printf "✅\n- Setting up target web application..." && \
 timeout 30s bash -c 'while [ "$(docker ps -a|grep http-auth-server)" = "" ];do printf ".";sleep 2s;done'   && \
 printf "✅\n- Setting up Jenkins..." && \
@@ -22,3 +24,4 @@ timeout 30s bash -c 'while [ "$(docker ps -a|grep conjur_server)" = "" ];do prin
 echo -e "✅\n- Ready! 😀"
 
 date
+

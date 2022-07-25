c37738df75a250f348b0942c95f87ebc45144cd5
Quincy Cheng <quincy.cheng@gmail.com>
2022-07-25 12:00:23 +0800

    cem jenkins

--- cem-jenkins/foreground.sh
+++ cem-jenkins/foreground.sh
@@ -4,9 +4,9 @@ set +H
 clear && \
 printf "Preparing environment, it will take 2-5mins...\n- Getting Jenkins files..." && \
 sleep 1s && \
-timeout 30s bash -c 'while ! [ -f /root/.clone_completed  ];do printf ".";sleep 2s;done'  && \
+timeout 120s bash -c 'while ! [ -f /root/.clone_completed  ];do printf ".";sleep 2s;done'  && \
 printf "✅\n- Getting Jenkins Image..." && \
-timeout 30s bash -c 'while [ "$(docker image ls | grep jenkins/jenkins)" = "" ];do printf ".";sleep 2s;done'   && \
+timeout 120s bash -c 'while [ "$(docker image ls | grep jenkins/jenkins)" = "" ];do printf ".";sleep 2s;done'   && \
 printf "✅\n- Setting up Jenkins..." && \
-timeout 30s bash -c 'while [ "$(docker ps -a|grep jenkins)" = "" ];do printf ".";sleep 2s;done'   && \
+timeout 120s bash -c 'while [ "$(docker ps -a|grep jenkins)" = "" ];do printf ".";sleep 2s;done'   && \
 echo -e "✅\n- Ready! 😀"

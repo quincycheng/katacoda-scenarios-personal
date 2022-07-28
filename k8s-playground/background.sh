7ff36acedeff2782b283f3b9972d324158a0d06a
Quincy Cheng <quincy.cheng@gmail.com>
2022-07-28 11:11:18 +0800

    k8s playground

--- k8s-playground/background.sh
+++ k8s-playground/background.sh
@@ -13,13 +13,15 @@ CMD ["ping", "killercoda.com"]
 EOF
 
 # Dictionary & Conjur
-apt install -y wamerican && apt install -y python3-pip && pip3 install conjur &
+#apt install -y wamerican && apt install -y python3-pip && pip3 install conjur &
 
 # Registry, Restart Docker, Helm
-docker run -d -p 5000:5000 --restart=always --name registry registry:2 &
-echo '172.30.1.2 local-registry' >> /etc/hosts && \
+#echo '172.30.1.2 local-registry' >> /etc/hosts && \
 echo '[[registry]]' >> /etc/containers/registries.conf && \
-echo 'location="local-registry:5000"' >> /etc/containers/registries.conf && \
+echo 'location="controlplane:5000"' >> /etc/containers/registries.conf && \
 echo 'insecure=true' >> /etc/containers/registries.conf  && \
-systemctl restart docker && \
+jq -S '. |= . + {"insecure-registries":[ "controlplane:5000" ]}' /etc/docker/daemon.json > /etc/docker/daemon.json && \
+systemctl restart docker 
+
+docker run -d -p 5000:5000 --restart=always --name registry registry:2 & 
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash &
--- k8s-playground/foreground.sh
+++ k8s-playground/foreground.sh
@@ -7,8 +7,6 @@ printf "✅\n- Installing Dictionary..." && \
 timeout 60s bash -c 'while ! [ -f /usr/share/dict/american-english ];do printf ".";sleep 2s;done'   && \
 printf "✅\n- Installing Helm..." && \
 timeout 60s bash -c 'while [ ! $(command -v helm) ];do printf ".";sleep 2s;done'   && \
-printf "✅\n- Installing Conjur CLI..." && \
-timeout 60s bash -c 'while [ ! $(command -v conjur) ];do printf ".";sleep 2s;done'   && \
 printf "✅\n- Setting up local registry..." && \
 timeout 120s bash -c 'while [ "$(docker ps -a|grep registry)" = "" ];do printf ".";sleep 2s;done'   && \
 echo -e "✅\n- Ready! 😀"

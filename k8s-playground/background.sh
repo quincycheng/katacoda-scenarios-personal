a6261ac9a3b226b525772122eb2dc6e41721df59
Quincy Cheng <quincy.cheng@gmail.com>
2022-07-28 12:18:32 +0800

    k8s playground

--- k8s-playground/background.sh
+++ k8s-playground/background.sh
@@ -26,12 +26,12 @@ EOF
 
 #sed -i '142 i New Line with sed' /etc/containerd/config.toml
 
-sed -i '142 i         [plugins."io.containerd.grpc.v1.cri".registry.mirrors."controlplane:5000"]' /etc/containerd/config.toml
-sed -i '143 i           endpoint = ["http://controlplane:5000"]' /etc/containerd/config.toml
-sed -i '144 i       ' /etc/containerd/config.toml
-sed -i '145 i       [plugins."io.containerd.grpc.v1.cri".registry.configs]' /etc/containerd/config.toml
-sed -i '146 i         [plugins."io.containerd.grpc.v1.cri".registry.configs."controlplane:5000".tls]' /etc/containerd/config.toml
-sed -i '147 i           insecure_skip_verify = true' /etc/containerd/config.toml
+sed -i '142 i \        [plugins."io.containerd.grpc.v1.cri".registry.mirrors."controlplane:5000"]' /etc/containerd/config.toml
+sed -i '143 i \          endpoint = ["http://controlplane:5000"]' /etc/containerd/config.toml
+sed -i '144 i \      ' /etc/containerd/config.toml
+sed -i '145 i \      [plugins."io.containerd.grpc.v1.cri".registry.configs]' /etc/containerd/config.toml
+sed -i '146 i \        [plugins."io.containerd.grpc.v1.cri".registry.configs."controlplane:5000".tls]' /etc/containerd/config.toml
+sed -i '147 i \          insecure_skip_verify = true' /etc/containerd/config.toml
 
 
 

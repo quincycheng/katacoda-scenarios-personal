6507bcf1017841ec77ca710b96475c1fa81dbdb2
Quincy Cheng <quincy.cheng@gmail.com>
2023-05-03 17:01:15 +0800

    Update foreground.sh

--- conjur-playground/foreground.sh
+++ conjur-playground/foreground.sh
@@ -5,8 +5,6 @@ clear && date && \
 printf "Preparing environment, it will take 2-5mins...\n- Getting container related files..." && \
 sleep 1s && \
 timeout 30s bash -c 'while ! [ -f /root/docker-compose.yml ];do printf ".";sleep 2s;done'  && \
-printf "✅\n- Installing Python PIP..." && \
-timeout 60s bash -c 'while [ ! $(command -v pip) ];do printf ".";sleep 2s;done'   && \
 printf "✅\n- Installing jq..." && \
 timeout 60s bash -c 'while [ ! $(command -v jq) ];do printf ".";sleep 2s;done'   && \
 printf "✅\n- Installing Conjur CLI..." && \
